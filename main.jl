871fcac8c3b3dbaff98f37bb63b71f7fed06a150
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-09-11 19:50:45 -0700

    restructuring

--- main.jl
+++ main.jl
@@ -1,1 +1,27 @@
 # main.jl
+using BenchmarkTools
+using CSV
+using DataFrames
+using LinearAlgebra
+using Revise
+using Symbolics
+
+rawDataFolder = "rawData/";
+filename = rawDataFolder*"FFD.csv";
+df = CSV.File(filename) |> DataFrame;
+rename!(df, [:t, :V])
+
+function objFun(df;getGradientToo=true)
+        @variables A₀, A, τ, ω, α, ϕ
+        x = [A₀, A, τ, ω, α, ϕ] 
+        fsym = A₀ + A*exp(-t/τ)sin((ω+α*t)t + ϕ)
+        f = (df.V - substitute.(fsym, t => df.t)).^2
+        ∇f = Symbolics.gradient.(f, x)
+        return f, ∇f
+end
+
+f, ∇f = objFun(df);
+
+fnum = build_function(f, x, expression=Val{false})
+∇fnum = build_function(∇f, x, expression=Val{false})
+

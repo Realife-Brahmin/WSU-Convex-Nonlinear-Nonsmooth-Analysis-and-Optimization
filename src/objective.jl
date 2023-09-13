d33f95c261e300276e4a32fe7cf6e9e2fa3b7b33
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-09-12 18:12:50 -0700

    doc added for objFun and it now generates average instead of sum

--- src/objective.jl
+++ src/objective.jl
@@ -4,7 +4,44 @@ using DataFrames
 using Symbolics
 
 # export objFun
+"""
+    objFun(df::DataFrame; getGradientToo=true, getFunctionsToo=true,
+        getVariables=true, verbose=false)
 
+Compute the objective function for a decaying sinusoidal model.
+
+# Arguments
+
+- `df::DataFrame`: A DataFrame with columns `t` and `V` where `t` represents time in seconds and `V` represents voltage in millivolts.
+
+# Keyword Arguments
+
+- `getGradientToo::Bool=true`: If true, the function will also compute and return the gradient of the objective function.
+
+- `getFunctionsToo::Bool=true`: If true, the function will also return the numerical (compiled) versions of the symbolic objective function (and its gradient if `getGradientToo` is true).
+
+- `getVariables::Bool=true`: Not used in the provided function code.
+
+- `verbose::Bool=false`: If true, will print out detailed information during the computation. Not implemented in the provided function code.
+
+# Returns
+
+- `f`: The symbolic representation of the objective function.
+
+- `∇f` (optional): The symbolic gradient of the objective function. Only returned if `getGradientToo=true`.
+
+- `fnum` (optional): The compiled numerical function of the objective function. Only returned if `getFunctionsToo=true`.
+
+- `∇fnum` (optional): The compiled numerical function of the gradient. Only returned if `getGradientToo=true` and `getFunctionsToo=true`.
+
+- `x`: The symbolic vector of parameters used in the model.
+
+# Example
+
+```julia
+df = DataFrame(t=[0.1, 0.2, 0.3], V=[0.5, 0.4, 0.3])
+f, ∇f, x = objFun(df)
+"""
 function objFun(df::DataFrame;
     getGradientToo::Bool=true, 
     getFunctionsToo::Bool=true,
@@ -17,7 +54,7 @@ function objFun(df::DataFrame;
     # @show typeof(x)
     # @show eltype(x)
     fsym = A₀ + A*exp(-t/τ)sin((ω+α*t)t + ϕ)
-    f = sum([(df.V[i] - substitute(fsym, t => df.t[i]))^2 for i in 1:N])
+    f = mean([(df.V[i] - substitute(fsym, t => df.t[i]))^2 for i in 1:N])
 
     if getGradientToo
         # Calculate the gradient for each squared difference

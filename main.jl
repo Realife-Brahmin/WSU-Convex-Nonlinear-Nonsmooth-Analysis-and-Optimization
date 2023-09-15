6027b8f2628340508cb7bd3ddb9ceed0d31fed0e
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-09-15 12:25:17 -0700

    algo seems to work, but is extremely slow
    Need to figure out the bottleneck(s)

--- main.jl
+++ main.jl
@@ -24,8 +24,9 @@ rename!(df, [:t, :V]);
 scatter_voltage_vs_time(df)
 
 alg = (method = "GradientDescent",
-        maxiter = 200,
+        maxiter = 2000,
         ngtol = 1e-8,
+        dftol = 1e-8,
         dxtol = 1e-8,
         lambda = 1,
         lambdaMax = 100,
@@ -35,6 +36,8 @@ alg = (method = "GradientDescent",
         progress = 10);
 
 functionName = "dampedSHM";
+
+x0 = [13.8, 8.3, 0.022, 1800, 900, 4.2];
 
 pr = (objective=functionName, x0=x0, alg=alg, df=df);
 
@@ -46,7 +49,6 @@ pr = (objective=functionName, x0=x0, alg=alg, df=df);
 # f, ∇f, fnum, ∇fnum, x = objFun(df);
 # x0_mine = estimate_x0(df, x)
 
-x0 = [13.8, 8.3, 0.022, 1800, 900, 4.2];
 # fnum([x0[1], 1, 1, 1, 1, 1])
 # fnum(x0_mine) # should be as close to 0.00 as possible
 # fnum(x0_Good)
@@ -55,9 +57,28 @@ x0 = [13.8, 8.3, 0.022, 1800, 900, 4.2];
 
 # t0 = df.t[1]
 # f, g = dampedSHM(x0, t0)
-F, G = computeCost(pr, x0)
-pk = findDirection(pr, G)
-α = linesearch(pr, x0, pk, verbose=true)
+Fnext = 1e10;
+F = computeCost(pr, x0, getGradientToo=false);
+x = pr.x0;
+itr = 1
+maxiter = pr.alg.maxiter
+Fvals = zeros(Float64, maxiter, 1)
+αvals = zeros(Float64, maxiter, 1)
+
+@profile begin
+        while abs(Fnext-F) ≥ pr.alg.dftol && itr ≤ pr.alg.maxiter 
+                println("Iteration $(itr):")
+                F, G = computeCost(pr, x)
+                println(F)
+                pk = findDirection(pr, G)
+                α, x, Fnext, backtracks = linesearch(pr, x, pk, verbose=false)
+                itr += 1
+        end
+end
+if itr > pr.alg.maxiter
+        @error("Failed to converge despite $(pr.alg.maxiter) iterations!")
+end
+
 # pk = findDirection(pr, g)
 # α = linesearch(pr, x0, t0, pk, verbose=true)
 

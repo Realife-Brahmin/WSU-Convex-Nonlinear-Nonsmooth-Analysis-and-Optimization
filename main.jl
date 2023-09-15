04df08d79455b851cf90949b51792dd33217f14a
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-09-15 04:01:48 -0700

    findDirection works

--- main.jl
+++ main.jl
@@ -7,6 +7,7 @@ using LaTeXStrings
 using LinearAlgebra
 using Plots
 using Revise
+using Statistics # for estimating x0, I'm too lazy to code it myself
 using Symbolics
 
 include("src/initializer.jl");
@@ -33,16 +34,25 @@ alg = (method = "GradientDescent",
         c2 = 0.9,
         progress = 10);
 
+functionName = "dampedSHM"
 
-f, ∇f, fnum, ∇fnum, x = objFun(df);
+pr = (objective=functionName, x0=x0, alg=alg)
 
+# pr = (alg=alg);
+pr == alg # returns true
+method = pr.alg.method
+# f, ∇f, fnum, ∇fnum, x = objFun(df);
 # x0_mine = estimate_x0(df, x)
 
-x0_Good = [13.8, 8.3, 0.022, 1800, 900, 4.2]
+x0 = [13.8, 8.3, 0.022, 1800, 900, 4.2]
 # fnum([x0[1], 1, 1, 1, 1, 1])
 # fnum(x0_mine) # should be as close to 0.00 as possible
-fnum(x0_Good)
+# fnum(x0_Good)
 # lol it got worse after I inserted a more sensible value of A?
+
+t0 = df.t[1]
+f, g = dampedSHM(x0, t0)
+findDirection(pr, g)
 
 
 

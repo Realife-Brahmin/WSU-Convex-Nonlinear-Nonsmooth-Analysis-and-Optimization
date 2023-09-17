775bc3bbfd205d490f726f3f6369b83693b13c00
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-09-17 01:22:27 -0700

    Stuff seems to work, but I want the code to be much faster.

--- main.jl
+++ main.jl
@@ -6,6 +6,7 @@ using Latexify
 using LaTeXStrings
 using LinearAlgebra
 using Plots
+using Profile
 using ProfileView
 using Revise
 using Symbolics
@@ -21,16 +22,28 @@ filename = rawDataFolder*"FFD.csv";
 df = CSV.File(filename) |> DataFrame;
 rename!(df, [:t, :V]);
 
+logging = true
+
+if logging
+        if !isdir("./logging")
+                println("Creating logging directory since it doesn't exist.")
+                mkdir("./logging")
+        else 
+                println("No need to create logging directory, it already exists.")
+                initialize_logging(overwrite=true)
+        end
+end
 scatter_voltage_vs_time(df)
 
 alg = (method = "GradientDescent",
-        maxiter = 2000,
+        maxiter = 200,
         ngtol = 1e-10,
         dftol = 1e-12,
         dxtol = 1e-10,
         lambda = 1,
         lambdaMax = 100,
-        linesearch = "Armijo",
+        # linesearch = "Armijo",
+        linesearch = "StrongWolfe",
         c1 = 1e-4, # Pg 33 (3.1 Step Length)
         c2 = 0.9,
         progress = 50);
@@ -42,45 +55,50 @@ x0 = [13.8, 8.3, 0.022, 1800, 900, 4.2];
 pr = (objective=functionName, x0=x0, alg=alg, df=df);
 
 
-dftol = pr.alg.dftol
+dftol = pr.alg.dftol;
 progress = pr.alg.progress;
 maxiter = pr.alg.maxiter;
 
 fnext = 1e10;
 fₖ = computeCost(pr, x0, getGradientToo=false);
 x = pr.x0;
-n = length(x)
-itr = 1
-fvals, αvals = [zeros(Float64, maxiter) for _ in 1:2]
+n = length(x);
+itr = 1;
+fvals, αvals = [zeros(Float64, maxiter) for _ in 1:2];
 backtrackVals = zeros(Int64, maxiter, 1);
 xVals = zeros(Float64, n, maxiter);
-# @profile begin
-        while abs(fnext-fₖ) ≥ pdftol && itr ≤ maxiter 
+
+println("Begin with the solver:")
+@profile begin
+        while abs(fnext-fₖ) ≥ dftol && itr ≤ maxiter
+                global fₖ, x, fnext, itr 
                 printOrNot = (itr%progress==0)
-                myprintln(printOrNot, "Iteration $(itr):")
+                myprintln(printOrNot, "Iteration $(itr):", log=true)
                 fₖ, ∇fₖ = computeCost(pr, x)
-                myprintln(printOrNot, fₖ)
+                myprintln(printOrNot, fₖ, log=true)
                 pₖ = findDirection(pr, ∇fₖ)
-                α, x, fnext, backtracks = linesearch(pr, x, pₖ, verbose=false)
+                α, x, fnext, backtrackNum = linesearch(pr, x, pₖ, verbose=true)
                 fvals[itr] = fnext
                 αvals[itr] = α
-                backtrackVals[itr] = backtracks
+                backtrackVals[itr] = backtrackNum
                 xVals[:, itr] = x
                 itr += 1
         end
-# end
+end
 if itr > maxiter
-        @error("Failed to converge despite $(maxiter) iterations!")
+        @warn ("Failed to converge despite $(maxiter) iterations!")
 else
+        println("Convergence achieved in $(itr) iterations 😄")
         fvals = fvals[1:itr]
         αvals = αvals[1:itr]
         backtrackVals = backtrackVals[1:itr]
 end
 
+# For testing linesearch
+# fₖ, ∇fₖ = computeCost(pr, x0)
+# pₖ = findDirection(pr, ∇fₖ)
+# linesearch(pr, x0, pₖ, verbose=true)
+
+ProfileView.view()
 # pₖ = findDirection(pr, g)
-# α = linesearch(pr, x0, t0, pₖ, verbose=true)
-
-
-
-
-
+# α = linesearch(pr, x0, pₖ, verbose=true)

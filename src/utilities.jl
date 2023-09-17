ebba5581a25785ad108d2593c9a828d7342f15f2
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-09-17 12:10:57 -0700

    A metaprogramming macro to automatically unpack fields from a NamedTuple (wait why does this call the variable as struc_var?)
    without even the need of specifying any fieldnames

--- src/utilities.jl
+++ src/utilities.jl
@@ -8,6 +8,16 @@ function safe_using(mod_name::Symbol)
     eval(:(using $mod_name))
 end
 
+# Extract fields from the 'res' structure and assign them to local variables.
+# The purpose of this macro is to avoid repetitive assignments.
+# Usage: @unpack_vars res status fvals αvals backtrackVals xvals
+macro unpack_vars(struct_var, fields...)
+    quote
+        # For each specified field, assign the field's value from the structure to a local variable.
+        $(Expr(:block, [:(local $(field) = $(struct_var).$(field)) for field in fields]...))
+    end
+end
+
 # safe_using(:Objective)
 
 

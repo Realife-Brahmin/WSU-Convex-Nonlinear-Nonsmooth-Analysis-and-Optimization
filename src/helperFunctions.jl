489121e59c8e2bb7ca1bd02f99c0513e98364119
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-09-17 01:21:58 -0700

    logging option added to all myprintln statements, very handy

--- src/helperFunctions.jl
+++ src/helperFunctions.jl
@@ -1,5 +1,19 @@
-function myprintln(verbose::Bool, message::Any)
-    if verbose
+function myprintln(print_flag::Bool, message; log::Bool=true, log_path::String="./logging/logs.txt")
+    if print_flag
         println(message)
+        if log
+            open(log_path, "a") do f  # append mode
+                println(f, message)
+            end
+        end
     end
 end
+
+function initialize_logging(;log_path::String="./logging/logs.txt", overwrite::Bool=false)
+    if overwrite
+        # Clear the old log by opening it in write mode and immediately closing it.
+        open(log_path, "w") do f end
+    end
+end
+
+

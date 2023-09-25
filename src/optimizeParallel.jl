9c5530251dcaab865c3b9a9ebcce06947a8f6318
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-09-25 07:12:18 -0700

    linesearch_parallel removed

--- src/optimizeParallel.jl
+++ src/optimizeParallel.jl
@@ -23,8 +23,9 @@ function optimizeParallel(pr; verbose::Bool=false, log::Bool=true, itrStart::Int
         myprintln(printOrNot, "Iteration $(itr):", log=true)
         fₖ, ∇fₖ = obj(x, p)
         pₖ = findDirection(pr, ∇fₖ)
-        # α, x, fnext, backtrackNum = linesearch(pr, x, pₖ, itrStart=itrStart)
-        α, x, fnext, backtrackNum = linesearch_parallel(pr, x, pₖ, itrStart=itrStart)
+        @warn "Line Search hasn't been parallelized. Only running non-parallelized Line Search."
+        α, x, fnext, backtrackNum = linesearch(pr, x, pₖ, itrStart=itrStart)
+        # α, x, fnext, backtrackNum = linesearch_parallel(pr, x, pₖ, itrStart=itrStart)
         fvals[itr] = fnext
         αvals[itr] = α
         backtrackVals[itr] = backtrackNum
@@ -49,49 +50,4 @@ function optimizeParallel(pr; verbose::Bool=false, log::Bool=true, itrStart::Int
     return res
 end
 
-function linesearch_parallel(pr::NamedTuple, xnow::Vector{Float64}, 
-    pₖ::Vector{Float64};
-    itrMax::Int64=50,
-    itrStart::Int64=1,
-    verbose::Bool=false,
-    log::Bool=true)
 
-    obj = pr.objective
-    p = pr.p
-    isStrongWolfe = (pr.alg.linesearch == "StrongWolfe")
-    c₁ = pr.alg.c1
-    fₖ, ∇fₖ = obj(xnow, p)
-    fnext = fₖ
-
-    # Atomic variable to store the result.
-    result = Threads.Atomic{Any}(nothing)
-
-    @threads for itr_search_for_α in itrStart:itrMax
-        # Check if result is already found by other thread
-        if isnothing(result[])
-            β = 1 / 2^(itr_search_for_α-1)
-            xnext = copy(xnow)
-            @inbounds xnext .= xnow .+ β .* pₖ
-            fnext, ∇fnext = obj(xnext, p)
-            comparison_val = fₖ + c₁ * β * dot(∇fₖ, pₖ)
-
-            if fnext ≤ comparison_val
-                if isStrongWolfe && abs(dot(∇fnext, pₖ)) < abs(c₁ * dot(∇fₖ, pₖ))
-                    continue
-                else
-                    # Update the result atomically
-                    Threads.atomic_cas!(result, nothing, (β, xnext, fnext, itr_search_for_α))
-                end
-            end
-        end
-    end
-
-    # Extract the result from atomic variable
-    res = result[]
-    if isnothing(res)
-        @error "Line Search failed at point x = $(xnow) despite $(itrMax) iterations."
-    else
-        α, x, f, backtracks = res
-        return (α=α, x=x, f=f, backtracks=backtracks)
-    end
-end

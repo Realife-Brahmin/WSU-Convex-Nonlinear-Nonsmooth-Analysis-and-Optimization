eb5e9cc4d18d06150d69428e812fd26ad259c957
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-09-28 17:41:51 -0700

    PyCall added
    let's put it to a spin

--- main.jl
+++ main.jl
@@ -24,9 +24,7 @@ include("src/objectiveParallel.jl");
 include("src/optimize.jl");
 include("src/optimizeParallel.jl");
 include("src/TestFunctions.jl");
-include("src/utilities.jl");
-
-
+include("src/utilities.jl"); 
 
 # global const JULIA_NUM_THREADS = 4;
 

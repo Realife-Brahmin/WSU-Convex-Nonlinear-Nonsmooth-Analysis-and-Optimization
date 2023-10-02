aa45acaf9aa2a933a75e36d8e32eb0b5d397fa26
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-10-01 20:14:53 -0700

    unused stuff removed

--- src/utilities.jl
+++ src/utilities.jl
@@ -1,13 +1,3 @@
-# import Base.eval
-
-function safe_import(mod_name::Symbol)
-    eval(:(import $mod_name))
-end
-
-function safe_using(mod_name::Symbol)
-    eval(:(using $mod_name))
-end
-
 macro unpack_vars(namedtuple_var)
     exprs = []
     fields = fieldnames(typeof(namedtuple_var))

97fe5cd69825014b9ba32e080abd340e6c1a4935
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-10-08 12:58:45 -0700

    packages broke, many packages removed

--- setup.jl
+++ setup.jl
@@ -5,23 +5,17 @@ using Base.Threads
 using BenchmarkTools
 using CSV
 using DataFrames
-using Latexify
 using LaTeXStrings
 using LinearAlgebra
-using Optim
 using Parameters
 using Plots
 using Printf
-using Profile
-using ProfileView
 using Revise
 
 include("src/display.jl");
 include("src/helperFunctions.jl");
 include("src/objective.jl");
-# include("src/objectiveParallel.jl");
 include("src/optimize.jl");
-# include("src/optimizeParallel.jl");
 include("src/TestFunctions.jl");
 include("src/TestFunctions_own.jl")
 include("src/utilities.jl"); 

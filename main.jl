25badd1d2412e48c5259fac24e2dac5c3440449b
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-10-09 08:29:38 -0700

    new, harder to solve functions added

--- main.jl
+++ main.jl
@@ -6,10 +6,12 @@ println("Your machine has a total of $(Sys.CPU_THREADS) available threads.")
 
 # functionName = "sphere"
 # functionName = "dampedSHM";
+# functionName = "Rastrigin2d";
 # functionName = "TestFunction1";
-functionName = "TestFunction2";
+# functionName = "TestFunction2";
 # functionName = "TestFunction3";
-# functionName = "rosenbrock";
+functionName = "rosenbrock";
+# functionName = "rosenbrock2d_oscillatory"
 
 pr = generate_pr(functionName);
 
@@ -22,6 +24,22 @@ benchmarking = false;
 @time res = optimize(pr, verbose=verbose)
 
 showresults(res, pr=pr)
+
+# Generate data for plotting
+x1_range = -2:0.05:2
+x2_range = -2:0.05:2
+
+f_values = [rosenbrock2d_oscillatory([x1, x2], empty_FuncParam(), getGradientToo=false) for x1 in x1_range, x2 in x2_range]
+# f_values = [Rastrigin2d([x1, x2], empty_FuncParam(), getGradientToo=false) for x1 in x1_range, x2 in x2_range]
+# f_values = [sphere([x1, x2], empty_FuncParam(), getGradientToo=false) for x1 in x1_range, x2 in x2_range]
+
+# Plot
+contour(x1_range, x2_range, f_values, legend=true, xlabel="x1", ylabel="x2")
+# contour(x1_range, x2_range, f_values, legend=true, xlabel="x1", ylabel="x2", title="Rastrigin2d")
+
+surface(x1_range, x2_range, f_values, xlabel="x1", ylabel="x2", zlabel="f(x1,x2)", title="3D Plot of rosenbrock2d_oscillatory")
+
+# contour(x1_range, x2_range, (x1, x2) -> x1^2 + x2^2)
 # plotresults(pr, res)
 
 # linesearchSW(pr, pr.x0, findDirection(pr, pr.objective(pr.x0, pr.p)[2]))
--- src/TestFunctions_own.jl
+++ src/TestFunctions_own.jl
@@ -1,3 +1,5 @@
+FuncParam = NamedTuple{(:params, :data), Tuple{Vector{Float64}, Matrix{Float64}}}
+
 function rosenbrock2d(x::Vector{Float64}, p::FuncParam; getGradientToo::Bool=true, verbose::Bool=false)
     # Ensure x is of length 2
     if length(x) != 2
@@ -42,3 +44,53 @@ function sphere(x::Vector{Float64}, p::FuncParam;
         return f
     end
 end
+
+function Rastrigin2d(x::Vector{Float64}, p::FuncParam;
+    getGradientToo::Bool=true,
+    verbose::Bool=false)
+
+    x1, x2 = x[1], x[2]
+    f = 10 * 2 + (x1^2 - 10cos(2π*x1)) + (x2^2 - 10cos(2π*x2))
+    if getGradientToo
+        g = zeros(Float64, 2)
+        g[1] = 2x1 + 20π*sin(2π*x1)
+        g[2] = 2x2 + 20π*sin(2π*x2)
+        return f, g
+    else
+        return f
+    end
+end
+
+function rosenbrock2d_oscillatory(x::Vector{Float64}, p::FuncParam; getGradientToo::Bool=true, verbose::Bool=false)
+    # Ensure x is of length 2
+    if length(x) != 2
+        throw(ArgumentError("Input vector x should be of length 2 for the 2D Rosenbrock function."))
+    end
+    
+    # Extract components
+    x1, x2 = x
+    
+    # Compute function value with oscillatory term
+    f = (1 - x1)^2 + 100*(x2 - x1^2)^2 + sin(5π*x1)^2
+
+    # If gradient is not needed, return only the function value
+    if !getGradientToo
+        return f
+    end
+    
+    # Compute gradient with derivative of the oscillatory term
+    dfdx1 = -2*(1 - x1) - 400*x1*(x2 - x1^2) + 2sin(5π*x1)*5π*cos(5π*x1)
+    dfdx2 = 200*(x2 - x1^2)
+    
+    g = [dfdx1, dfdx2]
+
+    # Optionally print the computed values
+    if verbose
+        println("Function value: $f")
+        println("Gradient: $g")
+    end
+
+    return f, g
+end
+
+

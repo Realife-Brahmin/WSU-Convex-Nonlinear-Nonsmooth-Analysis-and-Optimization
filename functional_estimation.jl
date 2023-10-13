f14705f3082c48deba2050e6704375b0b6eb5297
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-10-12 17:39:40 -0700

    kickstarting functional estimation project

---
+++ functional_estimation.jl
@@ -0,0 +1,9 @@
+include("setup.jl")
+
+println("You are currently using $(Threads.nthreads()) threads.")
+println("Your machine has a total of $(Sys.CPU_THREADS) available threads.")
+
+precision = 9
+
+
+

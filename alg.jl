b00f65b78e1f70a7db51fcf8a750dd7ea4b51257
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-10-22 10:57:16 -0700

    alg data structure now changed to mutable struct (::AlgorithmSettings)
    learned interesting OO things while converting it, including how julia is able to have a constructor function within the struct definition

--- alg.jl
+++ alg.jl
@@ -1,27 +1,53 @@
-alg = (
-        # method = "GradientDescent",
-        method = "QuasiNewton",
-        maxiter = Int(1e5),
-        # maxiter = Int(1e4),
-        # maxiter = Int(5),
-        # maxiter = Int(50),
-        gtol = 1e-10,
-        dftol = 1e-12,
-        # dftol = 1e-10,
-        # dftol = 1e-8,
-        dxtol = 1e-10,
-        lambda = 1,
-        lambdaMax = 100,
-        # linesearch = "Armijo",
-        linesearch = "StrongWolfe",
-        c1 = 1e-4, # Pg 33 (3.1 Step Length)
-        # c1 = 1e-2,
-        c2 = 0.9,
-        progress = 100
-        );
+mutable struct AlgorithmSettings
+        method::String
+        maxiter::Int
+        gtol::Float64
+        dftol::Float64
+        dxtol::Float64
+        lambda::Int
+        lambdaMax::Int
+        linesearch::String
+        c1::Float64
+        c2::Float64
+        progress::Int
 
-if alg.method == "QasiNewton"
-        alg.progress = 5
-elseif alg.method == "GradientDescent"
-        alg.progress = 100
+        # Constructor with default values
+        function AlgorithmSettings(; 
+                # method="ConjugateGradientDescent",
+                # method = "GradientDescent",
+                method = "QuasiNewton",
+                maxiter=Int(1e5),
+                gtol=1e-10,
+                dftol=1e-12,
+                dxtol=1e-10,
+                lambda=1,
+                lambdaMax=100,
+                linesearch="StrongWolfe",
+                c1=1e-4,
+                c2=0.9,
+                progress=100)
+
+                alg = new(method, maxiter, gtol, dftol, dxtol, lambda, lambdaMax, linesearch, c1, c2, progress)
+
+                if alg.method == "GradientDescent"
+                        alg.progress = 100
+                elseif alg.method == "QasiNewton"
+                        alg.linesearch = "StrongWolfe"
+                        alg.progress = 5
+                elseif alg.method == "ConjugateGradientDescent"
+                        alg.linesearch = "StrongWolfe"
+                        alg.c2 = 0.5
+                        alg.progress = 5
+                end
+
+                return alg
+        end
 end
+
+alg = AlgorithmSettings();
+#     # Example usage:
+#     alg = AlgorithmSettings()
+#     println(alg)
+
+#     alg2 = AlgorithmSettings(method="GradientDescent", progress=10)
+#     println(alg2)

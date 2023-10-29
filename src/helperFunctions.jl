065b9e3b255bfbc292b6272a391c2ec8f15960e9
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-10-28 20:43:10 -0700

    docs added for all current helper functions

--- src/helperFunctions.jl
+++ src/helperFunctions.jl
@@ -1,3 +1,10 @@
+"""
+    myprintln(print_flag::Bool, message; log::Bool=true, log_path::String="./logging/logs.txt")
+
+Print a message to the console if `print_flag` is `true`. Additionally, if `log` is `true`,
+the message will also be written to a log file specified by `log_path`. By default, the log path
+is set to `"./logging/logs.txt"`.
+"""
 function myprintln(print_flag::Bool, message; log::Bool=true, log_path::String="./logging/logs.txt")
     if print_flag
         println(message)
@@ -9,11 +16,24 @@ function myprintln(print_flag::Bool, message; log::Bool=true, log_path::String="
     end
 end
 
+
+"""
+    empty_FuncParam()::FuncParam
+
+Return an empty `FuncParam` object. The returned object has `params` set to an empty Float64 vector
+and `data` set to an empty Float64 matrix.
+"""
 function empty_FuncParam()::FuncParam
     return (params = Float64[], data = Matrix{Float64}(undef, 0, 0))
 end
 
 
+"""
+    @checkForNaN(varname)
+
+A macro that checks a vector for any NaN values. If NaN values are found,
+prints a message indicating the vector's name and the index where NaN is located.
+"""
 macro checkForNaN(varname)
     vec = esc(varname)
     quote
@@ -25,12 +45,25 @@ macro checkForNaN(varname)
     end
 end
 
-# @checkForNaN([-7, 0, NaN])
-# @checkForNaN([10, 20])
+
+"""
+    myzeros(array)
+
+Create a new array with the same size and type as the given `array`, but initialized with zeros.
+"""
 function myzeros(array)
     return zeros(eltype(array), size(array))
 end
+
+"""
+    myfill(array, value)
+
+Create a new array with the same size and type as the given `array`, but initialized with `value`.
+The type of the returned array's elements is preserved from the original array.
+"""
 function myfill(array, value)
     T = eltype(array)
     return fill(T(value), size(array))
 end
+
+

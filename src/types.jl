9d8c8865535af0422ea126cac1d2de646b2f6f57
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-11-02 18:22:02 -0700

    types.jl will now store all custom data structures
    two new containers: solState and solverState will now be used.

---
+++ src/types.jl
@@ -0,0 +1,23 @@
+mutable struct SolStateType
+    k::Int
+    fk::Float64
+    gk::Vector{Float64}
+    pk::Vector{Float64}
+    Hk::Matrix{Float64}
+    alphak::Float64
+    SolStateType() = new(0, 0.0, Float64[], Float64[], Matrix{Float64}(undef, 0, 0), 0.0)
+
+end
+
+mutable struct SolverStateType
+    k::Int
+    fevals::Int
+    gevals::Int
+    Hevals::Int
+    success_ls::Bool
+    SolverStateType() = new(0, 0, 0, 0, false)
+end
+
+# solState = SolStateType()
+
+# solverState = SolverStateType()

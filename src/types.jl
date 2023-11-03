e34e8bb617294133272d40d1453df7dabb4695f5
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-11-02 18:52:25 -0700

    gkmag a part of solState, and interpolationSteps (alpha_evals, current) now a part of solverState

--- src/types.jl
+++ src/types.jl
@@ -2,10 +2,11 @@ mutable struct SolStateType
     k::Int
     fk::Float64
     gk::Vector{Float64}
+    gkmag::Float64
     pk::Vector{Float64}
     Hk::Matrix{Float64}
     alphak::Float64
-    SolStateType() = new(0, 0.0, Float64[], Float64[], Matrix{Float64}(undef, 0, 0), 0.0)
+    SolStateType() = new(0, 0.0, Float64[], 0.0, Float64[], Matrix{Float64}(undef, 0, 0), 0.0)
 
 end
 
@@ -14,8 +15,9 @@ mutable struct SolverStateType
     fevals::Int
     gevals::Int
     Hevals::Int
+    alpha_evals::Int # only current evals
     success_ls::Bool
-    SolverStateType() = new(0, 0, 0, 0, false)
+    SolverStateType() = new(0, 0, 0, 0, 0, false)
 end
 
 # solState = SolStateType()

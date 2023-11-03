940392671b50d3eaa7bd89f4ac166b14015ea4e2
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-11-02 19:19:06 -0700

    Added minus1 variables to SolStateType
    forgot that they're required for checking convergence and SW conditions

--- src/types.jl
+++ src/types.jl
@@ -1,12 +1,28 @@
 mutable struct SolStateType
     k::Int
+    xkm1::Vector{Float64}
+    xk::Vector{Float64}
+    fkm1::Float64
     fk::Float64
+    gkm1::Vector{Float64}
     gk::Vector{Float64}
-    gkmag::Float64
+    gmagk::Float64
+    pkm1::Vector{Float64}
     pk::Vector{Float64}
+    Hkm1::Matrix{Float64}
     Hk::Matrix{Float64}
     alphak::Float64
-    SolStateType() = new(0, 0.0, Float64[], 0.0, Float64[], Matrix{Float64}(undef, 0, 0), 0.0)
+    
+    SolStateType() = new(
+        1, # k 
+        Float64[], Float64[], # xkm1, xk
+        0.0, 0.0,  # fkm1, fk
+        Float64[], Float64[], # gkm1, gk 
+        0.0, # gmagk
+        Float64[], Float64[], # pkm1, pk  
+        Matrix{Float64}(undef, 0, 0), Matrix{Float64}(undef, 0, 0), # Hkm1, Hk 
+        0.0 # alphak
+    )
 
 end
 
@@ -17,11 +33,12 @@ mutable struct SolverStateType
     Hevals::Int
     alpha_evals::Int # only current evals
     success_ls::Bool
-    SolverStateType() = new(0, 0, 0, 0, 0, false)
+
+    SolverStateType() = new(1, 0, 0, 0, 0, false)
 end
 
 # using Parameters
-# solState = SolStateType()
+solState = SolStateType()
 # @unpack fk, gk, Hk = solState
 # println("$fk, $gk, $Hk")
 # fk = 3.2; 
@@ -31,4 +48,4 @@ end
 # @unpack fk, gk, Hk = solState
 # println("$fk, $gk, $Hk")
 
-# solverState = SolverStateType()
+solverState = SolverStateType()

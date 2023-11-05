fcad5e371e698337a7025d3dd3aa7ffa1a387ad0
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-11-04 23:22:43 -0700

    function specification moved after verbose specification, paving the way for turning verbose based on function
    specifically so that drag function does not generate ugly long outputs for large n if I just wanna quickly test out my solver for various functions

--- main.jl
+++ main.jl
@@ -3,9 +3,18 @@ include("setup.jl")
 println("You are currently using $(Threads.nthreads()) threads.")
 println("Your machine has a total of $(Sys.CPU_THREADS) available threads.")
 
+verbose = false
+verbose = true;
+verbose_ls = false;
+verbose_ls = true;
+verbose_ls = verbose & verbose_ls
+logging = true;
+profiling = false;
+benchmarking = false;
+
 # functionName = "dampedSHM";
-# functionName = "drag"
-functionName = "rosenbrock";
+# functionName = "drag"; functionName == "drag" ? verbose = false : verbose = verbose  
+# functionName = "rosenbrock";
 # functionName = "sphere";
 # functionName = "TestFunction1";
 # functionName = "TestFunction2";
@@ -13,15 +22,6 @@ functionName = "rosenbrock";
 # functionName = "signalDenoise";
 
 pr = include("src/objfuns/"*String(functionName)*".jl")
-
-# verbose = false
-verbose = true;
-# verbose_ls = false;
-verbose_ls = true;
-verbose_ls = verbose & verbose_ls
-logging = true;
-profiling = false;
-benchmarking = false;
 
 @time res = optimize(pr, verbose=verbose, verbose_ls=verbose_ls)
 

41ada27805a7d0648784b01f605fa9bc922fce4d
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-11-06 14:26:06 -0800

    NamedTuples, now mutable, through this metaprogramming technique: `replace_field`
    All thanks to my friend.

--- src/utilities.jl
+++ src/utilities.jl
@@ -1,23 +1,13 @@
-# macro unpack_vars(namedtuple_var)
-#     exprs = []
-#     fields = fieldnames(typeof(namedtuple_var))
-#     for field in fields
-#         push!(exprs, :( $(Symbol(field)) = $namedtuple_var.$(Symbol(field)) ))
-#     end
-#     return esc(quote
-#         $(exprs...)
-#     end)
-# end
-
-# a = "aa2"
-# b = "baba2"
-# res1 = (a=a, b=b)
-# @unpack_vars res1
-# a
-# b
-# res1.a
-# res1.b
-
-# safe_using(:Objective)
+function replace_field(nt::NamedTuple, field::Symbol, value)
+    fields = fieldnames(typeof(nt))
+    values = map(fields) do f
+        if f == field
+            value
+        else
+            getproperty(nt, f)
+        end
+    end
+    return NamedTuple{fields}(values)
+end
 
 

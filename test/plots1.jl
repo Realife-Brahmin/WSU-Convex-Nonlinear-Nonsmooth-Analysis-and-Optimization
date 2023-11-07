76ce08750c67e72905b2a36d5dc40ed4103dc9f4
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-11-06 20:19:51 -0800

    nice plots

--- test/plots1.jl
+++ test/plots1.jl
@@ -1,5 +1,38 @@
 using Plots
 
-include("rawDataTest/medianDevSalaries.jl")
+include("rawDataTest/medianDevSalaries.jl"); # contains several data vectors, which can direcly be used for plotting
 
-plot(dev_x1, dev_y1)
+theme(:dracula)
+p1 = plot(dev_x1, dev_y1, dpi = 7000, 
+    xlabel = "Developer Age [years]",
+    ylabel = "Median Developer Salaries [\$]",
+    title = "Developer Salaries",
+    label = "Gen Developer Salaries",
+    color = :chocolate1,
+    # color = :slateblue,
+    # color = :gray2,
+    # color = :maroon3,
+    alpha = 1.0,
+    linestyle = :solid,
+    linewidth = 3.0)
+
+p2 = plot(p1, dev_x1, py_dev_y1, dpi = 7000, 
+label = "Python Developer Salaries",
+# color = :orchid,
+color = :teal,
+# color = :gray2,
+# color = :maroon3,
+alpha = 0.9,
+linestyle = :solid,
+linewidth = 4.5) 
+
+p3 = plot(p2, dev_x1, js_dev_y1, dpi = 7000, 
+label = "Javascript Developer Salaries",
+# color = :orchid,
+# color = :teal,
+color = :antiquewhite1,
+# color = :gray2,
+# color = :maroon3,
+linestyle = :dash,
+alpha = 0.8,
+linewidth = 2.5)

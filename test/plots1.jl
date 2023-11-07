c03ee94780f1733173ab27a66759f1909c50890f
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-11-06 20:33:09 -0800

    okay so themes have prespecified colors, no need of specifying my own

--- test/plots1.jl
+++ test/plots1.jl
@@ -2,37 +2,38 @@ using Plots
 
 include("rawDataTest/medianDevSalaries.jl"); # contains several data vectors, which can direcly be used for plotting
 
-theme(:dracula)
+# most liked themes: dao, dark, dracula, gruvbox_dark, gruvbox_light
+theme(:dao)
 p1 = plot(dev_x1, dev_y1, dpi = 7000, 
     xlabel = "Developer Age [years]",
     ylabel = "Median Developer Salaries [\$]",
     title = "Developer Salaries",
     label = "Gen Developer Salaries",
-    color = :chocolate1,
+    # color = :chocolate1,
     # color = :slateblue,
     # color = :gray2,
     # color = :maroon3,
-    alpha = 1.0,
+    # alpha = 1.0,
     linestyle = :solid,
     linewidth = 3.0)
 
 p2 = plot(p1, dev_x1, py_dev_y1, dpi = 7000, 
 label = "Python Developer Salaries",
 # color = :orchid,
-color = :teal,
+# color = :teal,
 # color = :gray2,
 # color = :maroon3,
-alpha = 0.9,
+# alpha = 0.9,
 linestyle = :solid,
-linewidth = 4.5) 
+linewidth = 3.0) 
 
 p3 = plot(p2, dev_x1, js_dev_y1, dpi = 7000, 
 label = "Javascript Developer Salaries",
 # color = :orchid,
 # color = :teal,
-color = :antiquewhite1,
+# color = :antiquewhite1,
 # color = :gray2,
 # color = :maroon3,
-linestyle = :dash,
-alpha = 0.8,
-linewidth = 2.5)
+linestyle = :solid,
+# alpha = 0.8,
+linewidth = 3.0)

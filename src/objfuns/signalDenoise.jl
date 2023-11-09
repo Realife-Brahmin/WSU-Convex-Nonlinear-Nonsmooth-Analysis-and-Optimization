683134645576cbbd02f955f2f1609b7d102e9a92
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-11-09 12:28:11 -0800

    Mistake in constructing xfull fixed.
    Was using d instead of x, so linesearches were actually always iterating on the same array.

--- src/objfuns/signalDenoise.jl
+++ src/objfuns/signalDenoise.jl
@@ -14,46 +14,48 @@ function signalDenoise(x::Vector{Float64},
     n = length(x)
     params = p.params
     d = p.data
-    if ~isempty(params) && length(params) == 5
-        p, α, β, L, R = params
+    if length(params) == 5
+        p, alpha, beta, L, R = params
     elseif length(params) == 3
         L = d[1]
         R = d[end]
-        p, α, β = params
+        p, alpha, beta = params
     else
         @error "Couldn't define parameters correctly."
     end
 
-    xfull = vcat(L, d, R)
+    xfull = vcat(L, x, R)
     
 
-    f = (1/2)*sum( (x-d).^2 )
-    @show xdiff = diff(xfull)[2:n]
+    f = (1/2n)*sum( (x-d).^2 )
+    @show xdiff = diff(xfull)
     if mod(p, 2) == 0
-        f += (α/p)*sum(xdiff.^p)
+        # println("Even integral p value.")
+        f += (alpha/(p*n)) * sum(xdiff.^p)
     else
-        f += (α/p)*sum( (xdiff.^2 + myfill(xdiff, β).^2).^(p/2) )
+        println("NOT even integral p value.")
+        f += (alpha/(p*n)) * sum( (xdiff.^2 + myfill(xdiff, beta).^2).^(p/2) )
     end
     
     if getGradientToo
         g = x - d
         if mod(p, 2) == 0
-            println("Even integral p value.")
+            # println("Even integral p value.")
             for i = 1:n
-                g[i] += α * (
-                    -(xdiff[i+1])^(p-1) 
-                    + xdiff[i]^p
-                    )
+                g[i] += alpha/n * (
+                    xdiff[i]^(p-1)
+                    -1*(xdiff[i+1])^(p-1) 
+                )
             end 
         else
             println("NOT even integral p value.")
             for i = 1:n
                 
                 # @show xdiff[i], xdiff[i+1]
-                g[i] += α *(
-                    -xdiff[i+1]*((-xdiff[i+1])^2 + β^2)^(p/2-1) 
-                    + xdiff[i]*(xdiff[i]^2 + β^2)^(p/2-1)
-                    )
+                g[i] += alpha/n *( 
+                        xdiff[i]*(xdiff[i]^2 + beta^2)^(p/2-1)
+                        -xdiff[i+1]*(xdiff[i+1]^2 + beta^2)^(p/2-1)
+                )
             end
         end
         return f, g
@@ -72,27 +74,28 @@ rename!(df, [:x, :y])
 
 
 # data = vec(df.y) # dampedSHM data
-data = [1.00, 1.01, 1.05, 0.93, 0.96, 1.10]; 
+# data = [1.00, 1.01, 1.05, 0.93, 0.96, 1.10]; 
 # data = Float64.(abs.(rand(Int8, 15)))
 # data = [1.00, 1, 1, 1, 1]
-# data = [1, 2, 3]
+data = [1, 2, 3]
+# data = [0.01, 0.02, 0.03]
 x0 = Float64.(data)
 
 # p = 0.5
 # p = 1
 p = 2
 
-# α = 0
-# α = 0.5
-# α = 1
-α = 100
+# alpha = 0
+alpha = 0.5
+# alpha = 1
+# alpha = 100
 
-β = 1e-5
+beta = 1e-5
 
 L = Float64[]
 R = Float64[]
 
-params = vcat([p, α, β], L, R)
+params = vcat([p, alpha, beta], L, R)
 objective = signalDenoise;
 
 pr = generate_pr(objective, x0, data=data, params=params)

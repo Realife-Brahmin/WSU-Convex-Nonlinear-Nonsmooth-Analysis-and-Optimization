61c8ea0642a6fecc57aad3654b0b7934abc25c0d
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-11-13 01:19:41 -0800

    slightly refactored for more specific usage

--- src/objfuns/construct_w0_from_dims.jl
+++ src/objfuns/construct_w0_from_dims.jl
@@ -1,4 +1,10 @@
 function construct_w0_from_dims(dims::Vector{Int})
+    numWeights = getNumWeights(dims)
+    w0 = ones(numWeights)
+    return w0
+end
+
+function getNumWeights(dims::Vector{Int})
     # Initialize the sum
     numWeights = 0
     
@@ -8,8 +14,7 @@ function construct_w0_from_dims(dims::Vector{Int})
         numWeights += dim[i] * dim[i+1]
     end
 
-    w0 = ones(numWeights)
-    return w0
+    return numWeights
 end
 
 # # Example usage:

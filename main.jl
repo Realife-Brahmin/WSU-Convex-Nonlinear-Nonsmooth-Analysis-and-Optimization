ff5c59cb849b01161539f5ffe23c5e4123513c8c
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-11-13 08:02:40 -0800

    a confusion matrix and heatmap has been added

--- main.jl
+++ main.jl
@@ -23,7 +23,7 @@ warmStart = true
 # functionName = "TestFunction2";
 # functionName = "TestFunction3";
 # functionName = "signalDenoise";
-# functionName = "nnloss";
+functionName = "nnloss";
 
 pr = include("src/objfuns/"*String(functionName)*".jl")
 
@@ -35,4 +35,33 @@ end
 
 showresults(res)
 
-plotresults(res, savePlot=true)
+if functionName == "nnloss"
+    w_trained = res.xvals[:, end]
+    classify = true
+    p = pr.p
+    p.params[:classify] = classify
+    # pr = replace_field(pr, :x0, w_trained)
+    # pr = replace_field(pr, :p, p)
+    y_pred_tr = vec(nnloss(w_trained, p))
+end
+y_tr = pr.p.params[:classData]
+
+# Binarize predictions based on the threshold
+threshold = 0.5
+binary_predictions = Int.(y_pred_tr .> threshold)
+binary_actual = Int.(y_tr .> threshold)  # Convert to Int if not already
+
+# Calculate the elements of the confusion matrix
+true_positives = sum((binary_predictions .== 1) .& (binary_actual .== 1))
+false_positives = sum((binary_predictions .== 1) .& (binary_actual .== 0))
+true_negatives = sum((binary_predictions .== 0) .& (binary_actual .== 0))
+false_negatives = sum((binary_predictions .== 0) .& (binary_actual .== 1))
+
+# Create the confusion matrix
+confusion_matrix = [true_positives false_positives;
+                    false_negatives true_negatives]
+
+# Plot the confusion matrix
+Plots.heatmap(["Actual Positive", "Actual Negative"], ["Predicted Positive", "Predicted Negative"], confusion_matrix, color=:blues, aspect_ratio=1)
+
+# plotresults(res, savePlot=true)

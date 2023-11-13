ac1549b911c8bec8c7e9e1bdf91e4efd2b305adf
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-11-12 20:37:07 -0800

    task 1: data retrieval and cleaning of p04 done
    Now onto making the actual loss function, starting with only classification

--- src/objfuns/nnloss.jl
+++ src/objfuns/nnloss.jl
@@ -36,12 +36,47 @@ function nnloss(w::Vector{Float64},
     end
 end
 
+import MLJ as MLJ
+# using  CSV
+# import DataFrames as DF
 
-rawDataFolder = "rawData/"
-filename = rawDataFolder * "Indian Liver Patient Dataset (ILPD).csv"
-df = CSV.File(filename) |> DataFrame
-rename!(df, [:x, :y])
+rawDataFolder = "rawData/";
+filename = rawDataFolder * "Indian Liver Patient Dataset (ILPD).csv";
+
+header = ["Age", "Gender", "TB", "DB", "Alkphos", "Sgpt", "Sgot", "TP", "ALB", "A/G Ratio", "Diagnosis"];
+
+df00 = CSV.File(filename, header=header) |> DataFrame;
+
+
+# df0 = DF.dropmissing(df00);
+df0 = dropmissing(df00);
+
+df = deepcopy(df0);
+# Convert "Gender" to 0 and 1
+df.Gender = map(gender -> gender == "Male" ? 0 : 1, df.Gender);
+
+# Convert "Diagnosis" from 1 to 1/3 and 2 to 2/3
+df.Diagnosis = map(diagnosis -> diagnosis == 1 ? 1/3 : 2/3, df.Diagnosis);
+
+# Normalize the first 10 columns
+for col in names(df)[1:10]  # Adjust the 10 here if the number of columns to normalize changes
+    min_val, max_val = minimum(df[!, col]), maximum(df[!, col]);
+    df[!, col] = map(x -> (x - min_val) / (max_val - min_val), df[!, col]);
+end
+
+# DF.describe(df)
+
+MLJ.schema(df)
+
+# vscodedisplay(df)
+
+df_train, df_test = MLJ.partition(df, 0.7, rng=123);
+
+yTrain, xTrain = MLJ.unpack(df_train, ==(:Diagnosis));
+
 data = Matrix(df)
+
+
 # x0 = [13.8, 8.3, 0.022, 1800, 900, 4.2]
 
 objective = nnloss;

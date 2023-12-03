44ba8e8a849be0f16bf5e099b9c430fbfcac8fae
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-12-02 16:20:06 -0800

    Just choosing colours for my heatmap :)

--- src/plotting/plotMinPathTimeTrajectory.jl
+++ src/plotting/plotMinPathTimeTrajectory.jl
@@ -8,10 +8,21 @@ function plotMinPathTimeTrajectory(res;
     p = pr[:p]
     params = p[:params]
     v = params[:v]
+    mx, my = size(v)
     A = params[:A]
     B = params[:B]
-    Plots.heatmap(v, yflip = true)
+    Am = 1 .+ (mx-1, my-1).*A
+    Bm = 1 .+ (mx-1, my-1).*B
+
+    Plots.heatmap(v, yflip = true, color = palette(:vik25, rev=false), clim = extrema(v))
+
+    # Add scatter points
+    scatter!([Am[1]], [Am[2]], color=:white, label="Point A")
+    scatter!([Bm[1]], [Bm[2]], color=:white, label="Point B")
+
     xlabel!("X-axis")
     ylabel!("Y-axis")
     title!("Speed Heatmap")
 end
+
+plotresults(res, savePlot=true)

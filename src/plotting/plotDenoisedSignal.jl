d576b90e99c143f0f7dfa88cd16546c204c6929e
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-12-12 22:23:13 -0800

    Trying a sizing scheme which works for my plots (no bogus spaces)
    I can see that size = (mx, my) can supersede any margins set.

--- src/plotting/plotDenoisedSignal.jl
+++ src/plotting/plotDenoisedSignal.jl
@@ -14,22 +14,31 @@ function plotDenoisedSignal(res;
     n = length(d)
     w = res.xvals[:, end]
     t = collect(1:1:n)
-    
+    minx, maxx = extrema(t)
+    miny, maxy = extrema(d)
+
     theme(:dao)
 
     p1 = Plots.scatter(t, d,
         title = "Denoised Signal \n "*"using "*L"p = "*"$(p) and α = $(alpha) \n"* "Method: $(method)",
         label = "data-point",
         xlabel = "Time Instance [unit]",
-        ylabel = "Signal value [unit]",
-        aspect = 1.0)
+        ylabel = "Signal value [unit]")
 
     p2 = Plots.plot!(p1, w,
         label = "fitted-function",
         linewidth = 3.0,
         linestyle = :solid,
         alpha = 0.5,
-        topmargin = 5mm)
+        # topmargin = 4mm,
+        # bottommargin = 2mm,
+        # leftmargin = -10mm,
+        # rightmargin = 10mm,
+        legend = :topright,
+        aspect_ratio = :equal,
+        size=(450,400), 
+        xlims=(minx*0.95, maxx*1.05), 
+        ylims=(miny*0.95, maxy*1.05))
 
     folderName = string(dirname(dirname(@__DIR__)))*"/processedData/"
     filename = folderName*"denoisedSignal_"*pr.alg.method*"_"*string(n)*"_p_$(replace(string(p), "." => "-"))_alpha_$(replace(string(alpha), "." => "-")).png"

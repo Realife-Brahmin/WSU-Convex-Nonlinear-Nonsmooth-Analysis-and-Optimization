768eeb810ca34f0b13408ceec034abb601da35dc
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-01-24 17:57:43 -0800

    nelderMead function runs, but reduces the simplex size?

--- src/nelderMead.jl
+++ src/nelderMead.jl
@@ -3,7 +3,7 @@ using Statistics
 include("helperFunctions.jl")
 include("sampleSpace.jl")
 
-function NelderMead(simplex, f::Function;
+function nelderMead(simplex, f::Function;
     alpha = 1.0,
     gamma = 2.0,
     beta = 0.5,
@@ -36,14 +36,38 @@ function NelderMead(simplex, f::Function;
         if F_xr < F_xsw 
             # point satisfies minimum required criterion for addition to simplex
             # just select it and move on
-            # simplex = insertSortedSimplex() 
+            !insertSortedSimplex!(simplex, xr, f)
         else
             F_xw = f(xw)
             if F_xr < F_xw
                 # this point is technically better than the worst point in the simplex, but not particularly useful. Can perform some contracts on it.
+                xoc = outsideContract(xc, xr, beta=beta)
+                F_xoc = f(xoc)
+                if F_xoc < F_xr
+                    # choosing outside contracted point (it may or may not be a useful addition to the simplex)
+                    action = "outsideContract"
+                    insertSortedSimplex!(simplex, xoc, f)
+                else
+                    # outside contract didn't help, but choosing reflected point anyway
+                    simplex = hcat(simplex[1:p-1], xr)
+                end
+            else
+                # oh no the point is even worse than the current worst point
+                # Let's try an inside contract
+                xic = insideContract(xc, xw, beta=beta)
+                action = "insideContract"
+                F_xic = f(xic)
+                if F_xic < F_xw
+                    # technically okay
+                    insertSortedSimplex!(simplex, xic, f)
+                else
+                    # okay no improvment this time
+                    # let's shrink the simplex
+                    shrinkSortedSimplex!(simplex, delta=delta)
+                    action = "shrink"
+                end
             end
-        end
-                
+        end             
     end
 
     return (simplex=simplex, action=action)
@@ -61,13 +85,30 @@ function extend(xc, xw;
     return xe
 end
 
+function outsideContract(xc, xr;
+    beta = 0.5)
+    xoc = xc + beta*(xr-xc)
+    return xoc
+end
+
+function insideContract(xc, xw;
+    beta = 0.5)
+    xic = xc + beta*(xw-xc)
+    return xic
+end
+
+function shrinkSortedSimplex!(simplex;
+    delta = 0.5)
+    simplex += delta*(simplex[1, :] .- simplex[:, :])
+    return simplex
+end
+
 """
     insertSortedSimplex!(matrix::Matrix{T}, new_vector::Vector{T}, f::Function) where T
 
 Insert a vector into a simplex (represented as a matrix with vectors as columns) 
 such that the columns remain sorted based on the value of a given function `f`, 
-applied to these vectors. After insertion, if the matrix exceeds its original 
-number of columns, the last column is removed to maintain a constant size.
+applied to these vectors. After insertion, the last column (represening the worst point) is removed to maintain a constant size.
 
 # Arguments
 - `matrix::Matrix{T}`: The simplex matrix with vectors as columns.
@@ -95,7 +136,7 @@ updated_simplex = insertSortedSimplex!(simplex, new_vector, f)
 # The updated simplex is now [1 3 7; 2 4 8] if f is sum of elements
 ```
 """
-function insertSortedSimplex!(matrix::Matrix{T}, new_vector::Vector{T}, f::Function) where T
+function insertSortedSimplex!(matrix, new_vector, f::Function)
     values = [f(matrix[:, i]) for i in 1:size(matrix, 2)]
     new_value = f(new_vector)
 
@@ -113,23 +154,30 @@ end
 
 
 # Define the function f()
-f(v) = sum(v)  # For example, f() could be the sum of the elements of the vector
+
+f(v) = sum(v.^2)  # For example, f() could be the sum of the elements of the vector
 
 # Create a sample simplex matrix
 simplex = Float64.([1 3 5; 2 4 6])  # 2x3 matrix, with vectors [1, 2], [3, 4], [5, 6] as columns
-
+println("originalSimplex:")
+display(simplex)
+v3 = [9, 12]
 # Define a new vector to insert
 new_vector = [2.5, 3.5]  # A new vector
 
 # Use the function to insert the new vector
-insertSortedSimplex!(simplex, new_vector, f)
+# insertSortedSimplex!(simplex, new_vector, f)
 v2 = [0.5, 2.3]
 # Display the updated simplex
-insertSortedSimplex!(simplex, v2, f)
-println(updated_simplex)
+# insertSortedSimplex!(simplex, v2, f)
+# insertSortedSimplex!(simplex, v3, f)
+simplex, action = nelderMead(simplex, f)
+println("Action performed: $(action)")
+println("Latest Simplex:")
+display(simplex)
 
 
 
-xc = [1, 2, 3]
-xw = [4, 5, 6]
+# xc = [1, 2, 3]
+# xw = [4, 5, 6]
 # xr = reflect(xc, xw, alpha=0.5)

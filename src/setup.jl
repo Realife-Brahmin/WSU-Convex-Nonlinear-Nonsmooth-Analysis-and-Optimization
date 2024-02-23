b72958fa5a16f4b515c62f2ceb159d3ce21467f5
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-02-23 00:06:49 -0800

    `setup.jl` now moved into `src/`

---
+++ src/setup.jl
@@ -0,0 +1,28 @@
+using Pkg
+Pkg.activate(".")
+Pkg.instantiate()
+
+using Base.Threads
+using BenchmarkTools
+using CSV
+using DataFrames
+using LaTeXStrings
+using LinearAlgebra
+using Parameters
+using Plots
+using Printf
+using Revise
+
+include("../alg.jl") # include alg.jl from root directory
+
+include("objfuns/objective.jl");
+include("display.jl");
+include("helperFunctions.jl");
+include("findDirection.jl");
+
+include("optimize.jl");
+include("optimize2.jl");
+
+include("plotting/plotresults.jl");
+include("types.jl");
+include("utilities.jl"); 

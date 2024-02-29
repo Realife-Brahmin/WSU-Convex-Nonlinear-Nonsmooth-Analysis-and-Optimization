abaf4ee44a812a456cd32a73ad2fedba72f67ed7
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-02-28 20:17:47 -0800

    skeleton written for `deriveNextGeneration`

--- src/deriveNextGeneration.jl
+++ src/deriveNextGeneration.jl
@@ -18,12 +18,18 @@ function deriveNextGeneration(Xk::Matrix, f::Function, pDict::Dict;
 
 
     # add the top survivor, it is assumed that Xk, Fk have the best values at the front
-    Xkp1[:, 1] = Xk[:, 1]
-    Fkp1[1] = Fk[1]
     popSize += 1
+    Xkp1[:, popSize] = Xk[:, 1]
+    Fkp1[popSize] = Fk[1]
+
     # TODO increment suitable actions for the top survivor
     
     while keepAddingToPopulation
+        
+        if popSize >= p
+            keepAddingToPopulation = false
+            myprintln(verbose, "Next Generation Complete.")
+        end
 
         p1, p2 = selectParents(Xk, Fk)
         actions[:parentsSelected] += 1
@@ -31,10 +37,24 @@ function deriveNextGeneration(Xk::Matrix, f::Function, pDict::Dict;
         o = crossover(p1, p2) # offspring
         actions[:crossover] += 1
 
-        om = mutate(o, f, pDict)
-        
+        om, mutations_1mut = mutate(o, f, pDict)
+        fevals += 1
+        actions[:mutations] += mutations_1mut
+
+        Xkp1, Fkp1, popSize, survived, actions_1dAA = decideAndAdd(p1, p2, om, Xkp1, Fkp1, popSize, pDict) # select the child, and if choosing to let the parent survive by default, the best min(2, p-popSize) parents
+        actions = merge(+, actions, actions_1dAA)
 
     end
 
-    return Xkp1, Fkp1, fevals_1GA, actions_1GA
+    Xkp1, Fkp1 = fittestFirst(Xkp1, Fkp1)
+
+    if Fkp1[1] > Fk[1]
+        actions[:genFitnessImproved] += 1
+        myprintln(verbose, "Fittest individual now even fitter.")
+    else
+        actions[:genFitnessNotImproved] += 1
+        myprintln(verbose, "Fittest individual has same fitness as previous generation.")
+    end 
+
+    return Xkp1, Fkp1, fevals, actions
 end

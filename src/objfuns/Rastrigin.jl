567ede2945b55015173611c401b0402438a29293
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-03-16 17:08:22 -0700

    `Rastigrin` written and tested. Is a hard function indeed.

--- src/objfuns/Rastrigin.jl
+++ src/objfuns/Rastrigin.jl
@@ -1,15 +1,34 @@
 include("objective.jl")
 
-function Rastrigin(x::Vector{Float64}, p; getGradientToo::Bool=false)
+function Rastrigin(x::Vector{Float64}, p;           
+    getGradientToo::Bool=true,
+    verbose::Bool=false)
+
     A = 10
     n = length(x)
-    sum_term = sum(x[i]^2 - A*cos(2*pi*x[i]) for i in 1:n)
-    f = A*n + sum_term + p[1]
+    # sum_term = sum(x[i]^2 - A*cos(2*pi*x[i]) for i in 1:n)
+    sum_term = sum(x.^2 - A*cos.(2*π*x))
+
+    f = A*n + sum_term
 
     if getGradientToo
-        g = [2*x[i] + 2*pi*A*sin(2*pi*x[i]) for i in 1:n]
+        # g = [2*x[i] + 2*pi*A*sin(2*pi*x[i]) for i in 1:n]
+        g = 2x + 2π*A*sin.(2π*x)
         return f, g
     else
         return f
     end
+
 end
+
+
+x0 = rand(7)
+# x = x0
+# p = Dict()
+# f, g = Rastrigin(x, p)
+
+params = Dict();
+
+objective = Rastrigin;
+
+pr = generate_pr(objective, x0, params=params);

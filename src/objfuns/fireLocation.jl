a421f8d2c2480f16cae95266ca26a321d2072314
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-04-03 18:48:56 -0700

    realized that I'm not using `fireLocation`, but rather only using that one time to get f0 and using a generic `equalityConstrainedQP` function in my `optimizeECQP`, so created it, which requires only a `w` and `G, c, A, b`.

--- src/objfuns/fireLocation.jl
+++ src/objfuns/fireLocation.jl
@@ -32,11 +32,43 @@ c_line = coords_y - m_line.*coords_x
 B = hcat(-m_line, -ones(4))
 m, n = size(B)
 d = c_line
+
+x0 = B \ d
+r0 = B * x0 - d
+w0 = vcat(x0, r0)
+
 params = Dict(:B=>B, :d=>d);
 
 objective = fireLocation;
 
-pr = generate_pr(objective, x0, params=params, problemType="ECQP")
+# pr = generate_pr(objective, x0, params=params, problemType="ECQP")
 
 #test run
-f0 = fireLocation(x0, params)
+f0 = fireLocation(x0, params, getGradientToo=false)
+
+# Henceforth, x, n, m lose their original meanings, in favour of w, n+m, n+m
+G = vcat(hcat(zeros(n, n), zeros(n, m)), hcat(zeros(m, n), I(m)));
+A = hcat(B, -I(m))
+c = zeros(n+m)
+
+pEQCP = Dict(:G=>G, :c=>c, :A=>A, :b=>b )
+
+function equalityConstrainedQP(w::Vector{Float64},
+    pDict;
+    verbose::Bool=false,
+    log::Bool=true,
+    getGradientToo::Bool=false)
+
+    @unpack G, A, c, b = pDict
+    f = 1//2*transpose(w)*G*w + transpose(w)*c
+
+    if getGradientToo
+        g = G*w + c
+        return f, g
+    else
+        return f
+    end
+    
+end
+
+solState = SolStatePGCGType(w0, G, A, c, fk=f0)

428dd3d2c29ccec1f574e8207d4e793555f3d0c5
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-04-04 21:27:48 -0700

    need to practice typing "transpose"

--- src/projectedGradientConjugateGradient.jl
+++ src/projectedGradientConjugateGradient.jl
@@ -1,19 +1,19 @@
 function solveForNextPGCGIterate(xk, gk, dk, rk, num, G, A, AAT;
         verbose=false)
 
-        den = tranpose(dk)*G*transpose(dk)
+        den = transpose(dk)*G*dk
 
         alphak = num/den
         xkp1 = xk + alphak*dk
         rkp1 = rk + alphak*G*dk
         vkp1 = AAT\(A*rkp1)
-        gkp1 = rkp1 - tranpose(A)*vkp1
+        gkp1 = rkp1 - transpose(A)*vkp1
         betak = transpose(rkp1)*gk/num
 
         dkp1 = -gkp1 + betak*dk
 
         fevals_1PGCG = 0
         actions_1PGCG = Dict()
-        
+
         return xkp1, gkp1, dkp1, rkp1, fevals_1PGCG, actions_1PGCG
 end

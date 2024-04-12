ce129ba0b40481289b44a0368b8498d72b79d871
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-04-11 21:17:27 -0700

    JuMP tutorial problem ezly solved, though it's an ECLP

--- src/objfuns/functionLP.jl
+++ src/objfuns/functionLP.jl
@@ -1,6 +1,9 @@
 include("objective.jl")
 
+using HiGHS
+using JuMP
 using Parameters
+
 
 function LPObjectiveFunction(x::Vector{Float64}, 
     pDict;
@@ -23,14 +26,25 @@ function LPObjectiveFunction(x::Vector{Float64},
     
 end
 
-pLP = Dict(:c => c, :Ae => Ae, :be => be, :Ai => Ai, :bi => bi)
+c = [1, 3, 5, 2]
+n = length(c)
+Ae = [1 1 9 5; 3 5 0 8; 2 0 6 13]
+be = [7, 3, 5]
+# pLP = Dict(:c => c, :Ae => Ae, :be => be, :A => A, :b => b)
 
-objective = LPObjectiveFunction
-objectiveOriginal = LPObjectiveFunction
-objectiveString = string(objectiveOriginal)
-params = pLP
+vector_model = Model(HiGHS.Optimizer)
+@variable(vector_model, x[1:n] >= 0)
+@constraint(vector_model, Ae * x .== be)
+@objective(vector_model, Min, c' * x)
+optimize!(vector_model)
+@assert is_solved_and_feasible(vector_model)
+objective_value(vector_model)
+# objective = LPObjectiveFunction
+# objectiveOriginal = LPObjectiveFunction
+# objectiveString = string(objectiveOriginal)
+# params = pLP
 
-pr = generate_pr(objective, w0, params=params, problemType="LP"; objectiveString=objectiveString)
+# pr = generate_pr(objective, w0, params=params, problemType="LP"; objectiveString=objectiveString)
 
 # f0 = equalityConstrainedQP(w0, pECQP)
 

66455901dfd4f77ed435db3c8932d2b26ad357d2
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-04-12 12:13:19 -0700

    Well since I can use an LP solver, I can use it to find a feasible point for a QP just as well. So maybe I don't have to solve it in the recommended way?

---
+++ src/objfuns/activeSetQuadraticProgramming.jl
@@ -0,0 +1,51 @@
+include("objective.jl")
+include("functionLP.jl")
+include("../solveLP.jl")
+
+using HiGHS
+using JuMP
+using Parameters
+
+function computeFeasiblePointForLinearConstraints(pDict;
+    )
+
+    @unpack E, I, Ae, be, A, b = pDict[:params]
+
+    n = size(Ae, 2)
+
+    vector_model = Model(HiGHS.Optimizer)
+    @variable(vector_model, x[1:n])
+    @constraint(vector_model, Ae * x .== be)
+    @constraint(vector_model, A * x .>= b)
+    optimize!(vector_model)
+    @assert is_solved_and_feasible(vector_model)
+    xopt = value.(x)
+
+    return xopt
+    
+end
+
+
+# c = [1, 3, 5, 2]
+# n = length(c)
+# x0 = rand(n)
+# Ae = [1 1 9 5; 3 5 0 8; 2 0 6 13]
+# be = [7, 3, 5]
+# E = length(be)
+# A = [0 0 -1 -1]
+# b = [-1]
+# I = length(b)
+# pLP = Dict(:c => c, :Ae => Ae, :be => be, :E => E, :A => A, :b => b, :I => I)
+# w0 = x0
+
+# objective = LPObjectiveFunction
+# # objectiveOriginal = LPObjectiveFunction
+# # objectiveString = string(objectiveOriginal)
+# objectiveString = "lpTestFunction1"
+# params = pLP
+
+# pr = generate_pr(objective, w0, params=params, problemType="LP"; objectiveString=objectiveString)
+
+# xf = computeFeasiblePointForLinearConstraints(pr.p)
+
+# xopt, fopt = solveLP(pr)

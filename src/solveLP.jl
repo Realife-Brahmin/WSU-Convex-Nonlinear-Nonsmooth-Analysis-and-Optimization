810de3e34429dac1d5455464d2af2f7f0a83d128
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-04-12 11:18:20 -0700

    a function to return `xopt, fopt` for an LP

---
+++ src/solveLP.jl
@@ -0,0 +1,22 @@
+function solveLP(pr;
+    verbose::Bool=false,
+    verbose_ls::Bool=false,
+    log::Bool=true,
+    log_path::String="./logging/")
+
+    @unpack c, Ae, be, A, b = pr.p[:params]
+    n = length(c)
+
+    vector_model = Model(HiGHS.Optimizer)
+    @variable(vector_model, x[1:n] >= 0)
+    @constraint(vector_model, Ae * x .== be)
+    @constraint(vector_model, A * x .>= b)
+    @objective(vector_model, Min, c' * x)
+    optimize!(vector_model)
+    @assert is_solved_and_feasible(vector_model)
+    xopt = value.(x)
+    fopt = objective_value(vector_model)
+
+    return xopt, fopt
+
+end

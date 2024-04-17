2cd83fe444b1a9263e5546fe399645122be167c8
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-04-16 19:01:12 -0700

    starting to write `optimizeASQP`

---
+++ src/optimizeASQP.jl
@@ -0,0 +1,128 @@
+
+include("activeSetQuadraticProgramming.jl")
+# include("projectedGradientConjugateGradient.jl")
+
+function optimizeASQP(pr;
+    verbose::Bool=false,
+    verbose_ls::Bool=false,
+    log::Bool=true,
+    log_path::String="./logging/")
+
+    objString = pr.objectiveString
+    log_txt = log_path * "log_" * objString * "_" * pr.alg[:method] * "_" * string(pr.alg[:maxiter]) * ".txt"
+
+    if isfile(log_txt)
+        rm(log_txt)
+    end # remove logfile if present for the run
+
+
+    solverState = SolverStateASQPType()
+
+    # @unpack genetic parameters = pr.alg
+
+    progress = pr.alg[:progress]
+    maxiter = pr.alg[:maxiter]
+    dftol = pr.alg[:dftol]
+    itol = pr.alg[:itol]
+
+    x0 = pr.x0
+    n = length(x0)
+    xk = x0
+
+    fvals = zeros(Float64, maxiter)
+    xvals = zeros(Float64, n, maxiter)
+
+    # doing this even though GA requires multiple f evals
+    myprintln(verbose, "Starting with initial point x = $(xk).", log_path=log_txt)
+    f = pr.objective
+    pASQP = pr.p
+    @unpack G, c, Ae, be, A, b = pASQP[:params]
+
+    f0 = f(x0, pASQP, getGradientToo=false)
+    fk = f0
+    solState = SolStateASQPType(x0, Ae, fk=f0, itol=itol)
+
+    @unpack fevals = solverState
+    fevals += 1
+    @pack! solverState = fevals
+
+    myprintln(verbose, "which has fval = $(fk)", log_path=log_txt)
+
+    keepIterationsGoing = true
+    causeForStopping = []
+
+    while keepIterationsGoing
+
+        @unpack k = solverState
+
+        @unpack xk, gk, dk, rk = solState
+        # saving the current iterates to solState
+        km1, xkm1, gkm1, dkm1, rkm1 = k, xk, gk, dk, rk
+        num = transpose(rk)*gk
+
+        if k >= maxiter
+            push!(causeForStopping, "Iteration limit reached!")
+            keepIterationsGoing = false
+            break
+        elseif num < tol
+            push!(causeForStopping, "Convergence Tolerance Reached")
+            keepIterationsGoing = false
+            break
+        end
+
+        @pack! solState = km1, xkm1, gkm1, dkm1, rkm1
+
+        printOrNot = verbose && ((k - 1) % progress == 0)
+        printOrNot_ECQP = printOrNot & verbose_ls
+
+        myprintln(printOrNot, "Iteration k = $(k)", log_path=log_txt)
+
+        @unpack actions, fevals = solverState
+
+        xkp1, gkp1, dkp1, rkp1, fevals_1PGCG, actions_1PGCG = solveForNextPGCGIterate(xk, gk, dk, rk, num, G, A, AAT, verbose=printOrNot_ECQP) # last two oargs are bogus
+
+        fkp1 = f(xkp1, pASQP, getGradientToo=false)
+        fevals += 1
+        fevals += fevals_1PGCG # bogus, does nothing
+        actions = merge(+, actions, actions_1PGCG) # bogus, does nothing
+
+        @pack! solverState = actions, fevals
+
+        # I prefer to only number a completed iteration, as opposed to numbering an in-process/about-to-begin iteration
+        k += 1
+
+        xvals[:, k] = xkp1
+        fvals[k] = fkp1 # also incorrect
+
+        xk, gk, dk, rk = xkp1, gkp1, dkp1, rkp1
+
+        @pack! solState = xk, gk, dk, rk
+
+        @pack! solState = k
+        @pack! solverState = k
+
+    end
+
+    @unpack k = solverState
+
+    if k ≥ maxiter
+        converged = false
+        statusMessage = "Failed to converge despite $(maxiter) iterations! 😢"
+        myprintln(true, statusMessage, log=log, log_path=log_txt)
+        @warn statusMessage
+    else
+        converged = true
+        statusMessage = "Convergence achieved in $(k) iterations 😄"
+        myprintln(true, statusMessage, log=log, log_path=log_txt)
+    end
+
+    @unpack fevals = solverState
+
+    res = (converged=converged, statusMessage=statusMessage, xvals=xvals, fvals=fvals, fevals=fevals, cause=causeForStopping, pr=pr, solState=solState,
+        solverState=solverState)
+
+    res = trim_array(res, k)
+
+    return res
+
+end

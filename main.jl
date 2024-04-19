f7fa05c9dfdefe8b0f36d612aacb34d80c42d4ca
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-04-18 19:03:05 -0700

    All functions have been retested with the latest `pDict` structure and seem to work fine (after numerous but tiny changes in each function and routine)

--- main.jl
+++ main.jl
@@ -3,8 +3,8 @@ include("src/setup.jl")
 println("You are currently using $(Threads.nthreads()) threads.")
 println("Your machine has a total of $(Sys.CPU_THREADS) available threads.")
 # revive this branch from my notebook
-# verbose = false
-verbose = true;
+verbose = false
+# verbose = true;
 verbose_ls = false;
 # verbose_ls = true;
 # verbose_ls = verbose & verbose_ls
@@ -18,7 +18,7 @@ warmStart = true
 # functionName = "dampedSHM";
 # functionName = "drag"; functionName == "drag" ? verbose = false : verbose = verbose  
 functionName = "fireLocation";
-# functionName = "nnloss";
+# functionName = "nnloss"; functionName == "nnloss" ? verbose = false : verbose = verbose;
 # functionName = "pathtime"
 # functionName = "receiverLocation"
 # functionName = "rosenbrock";

e561624111582c3a86382d285746be0d7aea4c8b
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-04-21 02:50:55 -0700

    Polyhedrons are nicely plotted for the `transmissionLines` problem. Now just need to insert scatter plots for each transmission tower location in them.

--- src/plotting/plotTransmissionLines.jl
+++ src/plotting/plotTransmissionLines.jl
@@ -1,7 +1,6 @@
 using Plots
 using Plots.PlotMeasures
 
-
 function plot_convex_hull!(points, label)
     x_coords = [p[1] for p in points]
     y_coords = [p[2] for p in points]
@@ -10,16 +9,47 @@ function plot_convex_hull!(points, label)
     scatter!(x_coords, y_coords, label="")
 end
 
-# Create an empty plot
-theme(:dao)
-plot(title="All Convex Hulls", aspect_ratio=:equal)
+function plotTransmissionLines(resVec;
+    savePlot=savePlot)
 
-# Plot each set of points
-plot_convex_hull!(P0, "P0")
-plot_convex_hull!(P1, "P1")
-plot_convex_hull!(P2, "P2")
-plot_convex_hull!(P3, "P3")
-plot_convex_hull!(P4, "P4")
 
-# Display the plot
-display(plot!())
+    res1 = resVec[1]
+    @unpack pr = res1
+    @unpack P = pr[:p]
+    
+    numPoly = length(resVec)
+    # P0, P1, P2, P3, P4 = 
+    theme(:dao)
+    p1 = plot(title="All Convex Hulls", aspect_ratio=:equal)
+
+    # Plot each set of points
+    for polyNum ∈ 0:numPoly
+        polyString = "P"*string(polyNum)
+        plot_convex_hull!(P[polyNum+1], polyString)
+    end
+
+    # plot_convex_hull!(P0, "P0")
+    # plot_convex_hull!(P1, "P1")
+    # plot_convex_hull!(P2, "P2")
+    # plot_convex_hull!(P3, "P3")
+    # plot_convex_hull!(P4, "P4")
+
+    # Display the plot
+    display(plot!())
+
+    folderName = string(dirname(dirname(@__DIR__)))*"/processedData/"
+    filename = folderName*"transmissionLines_"*pr.alg[:method]*".png"
+
+    if savePlot
+        
+        if isfile(filename)
+            rm(filename)
+        end
+    
+        Plots.savefig(p1, filename)
+    end
+
+    return p1
+
+end
+

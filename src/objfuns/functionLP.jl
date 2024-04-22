d8553eab8717aee6b503010bc7be97981a5b2eee
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-04-21 23:15:44 -0700

    :anchor:'d

--- src/objfuns/functionLP.jl
+++ src/objfuns/functionLP.jl
@@ -5,6 +5,44 @@ using HiGHS
 using JuMP
 using Parameters
 
+"""
+    LPObjectiveFunction(x::Vector{Float64}, pDict; verbose=false, log=true, getGradientToo=true)
+
+Evaluate the objective function and optionally its gradient for a linear programming problem.
+
+# Arguments
+- `x::Vector{Float64}`: The decision variable vector at which to evaluate the function.
+- `pDict::Dict`: A dictionary containing problem-specific data with key `:c` for the objective function's coefficients.
+
+# Keyword Arguments
+- `verbose::Bool=false`: Flag to enable verbose output.
+- `log::Bool=true`: Flag to enable logging of the function evaluations.
+- `getGradientToo::Bool=true`: Flag to indicate whether the gradient should also be returned.
+
+# Returns
+- `f::Float64`: The evaluated objective function value at `x`.
+- `g::Vector{Float64}`: (Optional) The gradient of the objective function at `x`.
+
+# Description
+This function computes the objective function value of a linear programming problem, which is simply the inner product of the coefficient vector `c` and the decision variable vector `x`. If the `getGradientToo` flag is set to `true`, it also returns the gradient, which in the case of a linear objective function, is constant and equal to `c`.
+
+# Notes
+- If `getGradientToo` is `false`, only the objective function value is returned.
+- This function will throw an error if neither of the conditions for `getGradientToo` is met.
+
+# Examples
+```julia
+# Define coefficients and decision variables
+c = [1.0, 2.0, 3.0]
+x = [4.0, 5.0, 6.0]
+pDict = Dict(:c => c)
+
+# Evaluate objective function value and gradient
+f, g = LPObjectiveFunction(x, pDict, getGradientToo=true)
+
+# The output will be the objective function value and the gradient vector
+```
+"""
 function LPObjectiveFunction(x::Vector{Float64}, 
     pDict;
     verbose::Bool=false,

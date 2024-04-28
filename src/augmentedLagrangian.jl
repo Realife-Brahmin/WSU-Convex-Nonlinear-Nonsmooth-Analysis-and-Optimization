caa4ec2035a5059faaccbd8f0d77c6061700935a
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-04-27 22:33:40 -0700

    these changes should make the function more in accodance with my standard notations

--- src/augmentedLagrangian.jl
+++ src/augmentedLagrangian.jl
@@ -9,8 +9,8 @@ using Parameters
 function ALOBJ(w, psubDict;
     getGradientToo::Bool=false)
 
-    @unpack lambdas, mu = psubDict
-    n = length(w) - psubDict.mI # length of x
+    @unpack lambda, mu, mE, mI = psubDict
+    n = length(w) - mI # length of x
     x = w[1:n]
     y = w[n+1:end]
 
@@ -18,14 +18,15 @@ function ALOBJ(w, psubDict;
     cE = psubDict.econ(x, psubDict, getGradientToo=false)
     cI = psubDict.icon(x, psubDict, getGradientToo=false)
 
-    Y = diagm(y)
-    c = vcat(cE, cI-Y*Y)
-    F = f - transpose(lambdas)*c + mu/2 * transpose(c) * c
+    # Y = diagm(y)
+    y2 = transpose(y)*y
+    c = vcat(cE, cI - y2)
+    F = f - transpose(lambda)*c + mu/2 * transpose(c) * c
 
     if !getGradientToo
         return F
     elseif getGradientToo
-        G = vcat(g, zeros(psubDict.mI)) - vcat( hcat(cE, cI), zeros(psubDict.mI, n) - 2*Y) * (lambdas - mu*c) # where are lambdas and mu coming from
+        G = vcat(g, zeros(mI)) - vcat( hcat(cE, cI), zeros(mI, n) - 2*y2) * (lambda - mu*c) # where are lambda and mu coming from
         return F, G
     else
         @error("floc")
